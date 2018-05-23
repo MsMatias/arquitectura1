30db1d6fb7220009411607e8ac90c6fef48fd638
MsMatias <matias.miranda.ms@gmail.com>
2018-05-23 18:02:15 -0300

    clave

--- main.c
+++ main.c
@@ -1,42 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+char* clave;
 
 int menu();
 int ingreso();
 void salida(unsigned char);
+void autofantastico();
+void delay(unsigned long int);
 
 int main(int argc, char *argv[]) {
 	
-	int estado_ingreso = 0;
+	clave = new char[6];
 	
-	while(estado_ingreso ){
-		estado_ingreso = ingreso();		
+	strcpy(clave, "123456");
+	
+	printf("Ingrese la clave: \n");
+	
+	if(ingreso()){
+		menu();
+	}else{
+		printf("3 intentos mal, chau");
 	}
+			
 	
-	menu();
-		
-	system("PAUSE");
 	
 	return 0;
 }
 
-int ingreso(){
+int ingreso(){	
 	
-	int
+	int contador = 0;
+	char* entrada;
+	char caracter;	
+	int c = 0;	
 	
+	entrada = new char[6];
+	c = 0;
+	
+	do{		
+		if(caracter = getch()){
+			printf("*");
+			if(caracter != 13){
+				*(entrada + c) = caracter;
+				c++;
+			}			
+		}
+			
+		
+	}while(caracter != 13);
+	
+	int comp = strcmp(clave, entrada);
+	
+	if(!comp){
+		return 1;
+	}
+		
 	return 0;
 }
+
+
 
 int menu (){
 	
 	system("CLS");
 	
 	int seleccion;
-	int dato;
+	int dato;	
 	
-
+	do{
+	
 	printf("--------------MENU---------------\n");
 	printf("1) Auto fantastico\n");
 	printf("2) La carrera\n");
@@ -55,6 +92,7 @@ int menu (){
 		
 		case 1:
 			printf("Seleccionaste auto fantastico \n");
+			autofantastico();
 		break;
 		
 		case 2:
@@ -77,21 +115,45 @@ int menu (){
 		
 	}	
 
-	system("PAUSE");
-	menu();
+	}while(1);
 }
 
 void salida(unsigned char c){
 	
-	int b = 0;
+
 	int i = 0;
 	
-	for(i = 0; i < 8; i++){
-		b = (c >> i) & 1;
-		if(b == 1){
-			printf(" * ");
-		}else{
-			printf(" _ ");
-		}
+	for(i = 7; i >= 0; i--){
+		if((c >> i) & 1){
+			system("color");		
+			printf("*");			
+		}else
+			printf("_");		
 	}
+}
+
+void autofantastico(){
+	
+	int valor = 0;
+	
+	do{
+	
+			
+		for(int i = 1; i < 128; i = i<<1){
+			system("CLS");
+			salida(i);
+			delay(10000000);
+		}
+			
+		for(int i = 128; i > 1; i = i>>1){
+			system("CLS");
+			salida(i);
+			delay(10000000);
+		}
+			
+	}while(!kbhit());
+}
+
+void delay(unsigned long int a){
+	while(a) a--;
 }
